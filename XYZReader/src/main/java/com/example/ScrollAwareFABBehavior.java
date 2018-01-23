66fcb5c0d8913886a050bd5d9f286e5c071e0328
Momen Ali <engmomenali@gmail.com>
2018-01-23 18:17:19 +0200

    commit after adding the Fab behavior

---
+++ XYZReader/src/main/java/com/example/ScrollAwareFABBehavior.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by Momen Ali on 1/23/2018.
+ */
+
+public class ScrollAwareFABBehavior extends CoordinatorLayout.Behavior<FloatingActionButton>  {
+    private static final String TAG  = "ScrollAwareFABBehavior";
+    public ScrollAwareFABBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public boolean onStartNestedScroll(final CoordinatorLayout coordinatorLayout, final FloatingActionButton child,
+                                       final View directTargetChild, final View target, final int nestedScrollAxes) {
+        return true;
+    }
+
+    @Override
+    public void onNestedScroll(final CoordinatorLayout coordinatorLayout,
+                               final FloatingActionButton child,
+                               final View target, final int dxConsumed, final int dyConsumed,
+                               final int dxUnconsumed, final int dyUnconsumed) {
+        Log.d(TAG, "onNestedScroll: " + dyConsumed);
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed,dxUnconsumed, dyUnconsumed);
+
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            child.hide();
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            child.show();
+        }
+    }
+}
